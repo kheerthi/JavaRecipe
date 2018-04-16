793e78e477cbd7ee805cb9403b8eead8e6cbf3bf
kheerthi <kheerthi.m@gmail.com>
2018-04-16 15:13:01 -0500

    Update Install.rb
    added bin_cmds: array of Java tool names to set or unset alternatives on.

--- recipes/Install.rb
+++ recipes/Install.rb
@@ -1,5 +1,6 @@
 windows_package node['name']['JDK1.7']  do
     source                  node['source']['JDK1.7']
+    bin_cmds ["java", "javac"]
     action                  :install
     installer_type          :custom
     options     "/s INSTALLDIR=\"#{node['path']['JDK1.8'].gsub('/','\\')}\""
