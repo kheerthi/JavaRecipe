576037453e5d53b714c204daaf740610091f2b62
kheerthi <kheerthi.m@gmail.com>
2018-04-15 21:49:21 -0500

    Create metadata.rb
    version information

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'java8'
+license          'all_rights'
+description      'Installs/Configures java8'
+long_description 'Installs/Configures java8'
+version          '0.1.0'
+
+depends 'java'
